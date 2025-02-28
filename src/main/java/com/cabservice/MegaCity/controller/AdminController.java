81026e3b8b094b79b36e748ee8b1018f5450acef
PasanSasmika <pasansasmika198@gmail.com>
2025-02-28 08:17:33 +0530

    content update

--- src/main/java/com/cabservice/MegaCity/controller/AdminController.java
+++ src/main/java/com/cabservice/MegaCity/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,8 @@ import com.cabservice.MegaCity.service.AdminService;
 
 @RestController
  @CrossOrigin(origins = "*")
+ @RequestMapping("/api/admin")
+
 public class AdminController {
     
    @Autowired
@@ -23,14 +26,14 @@ public class AdminController {
         private PasswordEncoder passwordEncoder;
 
 
-    @PostMapping("/auth/createadmin")
+    @PostMapping("/createadmin")
     @ResponseStatus(HttpStatus.CREATED)
     public Admin createAdmin(@RequestBody Admin admin) {
         admin.setPassword(passwordEncoder.encode(admin.getPassword()));
         return adminService.createAdmin(admin);
     }
 
-    @GetMapping("/{adminID}")
+    @GetMapping("/auth/alladmins/{adminID}")
     public Admin getAdminId(@PathVariable String adminID) {
         return adminService.getAdminById(adminID);
     }
